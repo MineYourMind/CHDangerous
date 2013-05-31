732e9532a7f1b1cc4f9098419f8204cf7501a308
Chris Gallarno <cgallarno200@gmail.com>
2013-05-31 17:50:22 -0400

    Inital Commit added file_write

---
+++ src/main/java/com/brothercraft/chdangerous/CHDangerous.java
@@ -0,0 +1,93 @@
+package com.brothercraft.chdangerous;
+
+import com.laytonsmith.PureUtilities.TermColors;
+import com.laytonsmith.PureUtilities.Version;
+import com.laytonsmith.annotations.api;
+import com.laytonsmith.annotations.startup;
+import com.laytonsmith.core.CHVersion;
+import com.laytonsmith.core.Security;
+import com.laytonsmith.core.Static;
+import com.laytonsmith.core.constructs.CVoid;
+import com.laytonsmith.core.constructs.Construct;
+import com.laytonsmith.core.constructs.Target;
+import com.laytonsmith.core.environments.Environment;
+import com.laytonsmith.core.exceptions.ConfigRuntimeException;
+import com.laytonsmith.core.functions.AbstractFunction;
+import com.laytonsmith.core.functions.Exceptions;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.logging.Level;
+
+public class CHDangerous {
+
+	@startup
+	public static void onEnable(){
+	    System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "");
+	    System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "============================ [CHDangerous] ============================ ");
+	    System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "[WARNING]: CHDangerous contains functions that if used incorrectly can ruin your life.");
+	    System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "PLEASE make sure you know what you are doing.");
+	    System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "ANY DAMAGE DONE IS SOLEY THE RESPONSIBILITY OF THE SERVER OWNER / MAINTAINER.");
+	    System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "ANYTHING CONTAINED IN THIS EXTENSION IS NOT GUARANTEED TO EVEN DO WHAT IT WAS DESIGNED TO DO");
+	    System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "=======================================================================" + TermColors.BLINKOFF + TermColors.RESET);
+	    System.out.println(" ");
+	}
+
+	@api
+	public static class file_write extends AbstractFunction {
+
+	public Exceptions.ExceptionType[] thrown() {
+		return new Exceptions.ExceptionType[]{Exceptions.ExceptionType.IOException, Exceptions.ExceptionType.SecurityException};
+	}
+
+	public boolean isRestricted() {
+	    return true;
+	}
+
+	public Boolean runAsync() {
+	    return true;
+	}
+
+	public Construct exec(Target t, Environment environment, Construct... args) throws ConfigRuntimeException {
+			String location = args[0].val();
+			String content = args[1].val();
+			location = new File(t.file().getParentFile(), location).getAbsolutePath();
+			//Verify this file is not above the craftbukkit directory (or whatever directory the user specified
+			if (!Security.CheckSecurity(location)) {
+				throw new ConfigRuntimeException("You do not have permission to access the file '" + location + "'",
+					Exceptions.ExceptionType.SecurityException, t);
+			}
+			try {
+			    File file = new File(location);
+			    FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			    fw.write(content);
+			    fw.close();
+			    return new CVoid(t);
+			} catch (Exception ex) {
+				Static.getLogger().log(Level.SEVERE, "Could not write in file while attempting to find " + new File(location).getAbsolutePath()
+					+ "\nFile " + (new File(location).exists() ? "exists" : "does not exist"));
+				throw new ConfigRuntimeException("File could not be written in.",
+					Exceptions.ExceptionType.IOException, t);
+			}
+		}
+
+	public String getName() {
+	    return "file_write";
+	}
+
+	public Integer[] numArgs() {
+	    return new Integer[]{2};
+	}
+
+	public String docs() {
+	    return "String {file}, String {text} Writes to a file from the file system at location var1 with the text in var2. The path is relative to"
+		+ " the file that is being run, not CommandHelper. If the file is not found, or otherwise can't be read in, an IOException is thrown."
+		+ " If the file specified is not within base-dir (as specified in the preferences file), a SecurityException is thrown.";
+	}
+
+	public Version since() {
+	    return CHVersion.V3_3_1;
+	}
+	    
+	}
+
+}
