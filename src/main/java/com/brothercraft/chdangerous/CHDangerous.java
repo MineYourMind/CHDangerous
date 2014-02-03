b88fbdbb704c3128556be2e61e92a4202ed5a0c5
Cailin Smith <savannahcailin@gmail.com>
2014-02-03 11:02:57 -0600

    Update CHDangerous to use new format.

---
+++ src/main/java/com/brothercraft/chdangerous/CHDangerous.java
@@ -0,0 +1,19 @@
+
+package com.brothercraft.chdangerous;
+
+import com.laytonsmith.PureUtilities.SimpleVersion;
+import com.laytonsmith.PureUtilities.Version;
+import com.laytonsmith.core.extensions.AbstractExtension;
+import com.laytonsmith.core.extensions.MSExtension;
+
+/**
+ *
+ */
+@MSExtension("CHDangerous")
+public class CHDangerous extends AbstractExtension {
+
+	public Version getVersion() {
+		return new SimpleVersion(1, 0, 0);
+	}
+
+}
