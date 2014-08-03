a36a5f603e84d77b10eab6ab936bf7b9153499e0
slind14 <CXLBDkVdgGxGnhf9B2o0>
2014-08-03 11:47:15 +0200

    sorry, the on enable messages are too heavy during debugging phase.

--- src/main/java/com/brothercraft/chdangerous/CHDangerous.java
+++ src/main/java/com/brothercraft/chdangerous/CHDangerous.java
@@ -19,14 +19,6 @@ public class CHDangerous extends AbstractExtension {
 
 	@Override
 	public void onStartup() {
-		System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "");
-		System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "============================ [CHDangerous] ============================ ");
-		System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "[WARNING]: CHDangerous contains functions that if used incorrectly can ruin your life.");
-		System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "PLEASE make sure you know what you are doing.");
-		System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "ANY DAMAGE DONE IS SOLEY THE RESPONSIBILITY OF THE SERVER OWNER / MAINTAINER.");
-		System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "ANYTHING CONTAINED IN THIS EXTENSION IS NOT GUARANTEED TO EVEN DO WHAT IT WAS DESIGNED TO DO");
-		System.out.println(TermColors.BLINKON + TermColors.BG_RED + TermColors.BRIGHT_WHITE + "=======================================================================" + TermColors.BLINKOFF + TermColors.RESET);
-		System.out.println(" ");
 	}
 	
 	
